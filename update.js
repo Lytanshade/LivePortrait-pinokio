61770e41161c66aaf50d234505ee1b74227a1557
Lytan <116470525+Lytanshade@users.noreply.github.com>
2024-07-12 16:30:51 +1200

    check/install requirements on update

--- update.js
+++ update.js
@@ -9,6 +9,13 @@ module.exports = {
     params: {
       path: "app",
       message: "git pull"
+	}
+  }, {
+     method: "shell.run",
+     params: {
+       venv: "env",
+      message: "pip install -r requirements.txt",
+      path: "app"
     }
   }]
 }
