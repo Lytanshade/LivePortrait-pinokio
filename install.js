d403ded90be5630429c2818e117b1e88610b6e88
Lytan <116470525+Lytanshade@users.noreply.github.com>
2024-07-18 04:50:50 +1200

    +macOS silicon

--- install.js
+++ install.js
@@ -28,8 +28,9 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install gradio devicetorch",
-          "pip install -r requirements.txt"
+           "pip install gradio devicetorch",
+           "{{platform === 'darwin' ? 'pip install onnxruntime-silicon==1.16.3'}}",
+           "pip install -r ../requirements.txt",
         ]
       }
     },
