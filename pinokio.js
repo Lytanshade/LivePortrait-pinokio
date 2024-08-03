eab4a39f3081bf4d1a7584d5c51d97bfecd4b30b
Lytan <116470525+Lytanshade@users.noreply.github.com>
2024-08-03 17:52:59 +1200

    Add files via upload

--- pinokio.js
+++ pinokio.js
@@ -1,24 +1,31 @@
 const path = require('path')
 module.exports = {
-  version: "1.5",
+  version: "2.0",
   title: "LivePortrait",
   description: "LivePortrait - animals update: 6GB VRAM & ~8GB download",
   icon: "icon.png",
-  menu: async (kernel) => {
-    let installing = await kernel.running(__dirname, "install.js")
-    let installed = await kernel.exists(__dirname, "app", "env")
-    let running = await kernel.running(__dirname, "start.js")
-    if (installing) {
+  menu: async (kernel, info) => {
+    let installed = info.exists("env")
+    let running = {
+      install: info.running("install.js"),
+      start: info.running("start.js"),
+	  start2: info.running("start_animals.js"),
+      update: info.running("update.js"),
+      reset: info.running("reset.js")
+    }
+    if (running.install) {
       return [{
+        default: true,
         icon: "fa-solid fa-plug",
         text: "Installing",
         href: "install.js",
       }]
     } else if (installed) {
-      if (running) {
-        let local = kernel.memory.local[path.resolve(__dirname, "start.js")]
+      if (running.start) {
+        let local = info.local("start.js")
         if (local && local.url) {
           return [{
+            default: true,
             icon: "fa-solid fa-rocket",
             text: "Open Web UI",
             href: local.url,
@@ -29,16 +36,57 @@ module.exports = {
           }]
         } else {
           return [{
+            default: true,
             icon: 'fa-solid fa-terminal',
             text: "Terminal",
             href: "start.js",
           }]
         }
+      } else if (running.start2) { 
+        let local = info.local("start_animals.js") 
+        if (local && local.url) {
+          return [{
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start_animals.js",
+          }]
+        } else {
+          return [{
+            default: true,
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start_animals.js",
+          }]
+        }
+      } else if (running.update) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Updating",
+          href: "update.js",
+        }]
+      } else if (running.reset) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Resetting",
+          href: "reset.js",
+        }]
       } else {
         return [{
+          default: true,
           icon: "fa-solid fa-power-off",
           text: "Start",
           href: "start.js",
+        }, {
+          icon: "fa-solid fa-power-off",
+          text: "Start - Animals",
+          href: "start_animals.js",
         }, {
           icon: "fa-solid fa-plug",
           text: "Update",
@@ -47,10 +95,15 @@ module.exports = {
           icon: "fa-solid fa-plug",
           text: "Install",
           href: "install.js",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
         }]
       }
     } else {
       return [{
+        default: true,
         icon: "fa-solid fa-plug",
         text: "Install",
         href: "install.js",
