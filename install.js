3356d040f95ee4bc65aeaabe94993775f0f0af94
Lytan <116470525+Lytanshade@users.noreply.github.com>
2024-08-03 17:06:23 +1200

    Update install.js

--- install.js
+++ install.js
@@ -1,133 +1,101 @@
 module.exports = {
-  run: [
-    // Edit this step to customize the git repository to use
-    {
-      method: "shell.run",
-      params: {
-        message: [
-          "git clone --single-branch --branch animals https://github.com/Lytanshade/LivePortrait.git app ",
-        ]
-      }
-    },
-    // Delete this step if your project does not use torch
-    {
-      method: "script.start",
-      params: {
-        uri: "torch.js",
-        params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-        }
-      }
-    },
-    // Edit this step with your custom install commands
-    {
-      method: "shell.run",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "pip install gradio devicetorch",
-          "pip install -r {{platform==='darwin' ? 'requirements_macOS.txt' : 'requirements.txt'}}"
-        ]
-      }
-    },
-    //  Uncomment this step to add automatic venv deduplication (Experimental)
-    //  {
-    //    method: "fs.link",
-    //    params: {
-    //      venv: "app/env"
-    //    }
-    //  },
-	
-	 {
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/insightface/resolve/main/det_10g.onnx",
-		dir: "app/pretrained_weights/insightface/models/buffalo_l"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/insightface/resolve/main/2d106det.onnx",
-		dir: "app/pretrained_weights/insightface/models/buffalo_l"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/landmark.onnx",
-		dir: "app/pretrained_weights/liveportrait"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/base_models/appearance_feature_extractor.pth",
-		dir: "app/pretrained_weights/liveportrait/base_models"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/base_models/motion_extractor.pth",
-		dir: "app/pretrained_weights/liveportrait/base_models"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/base_models/spade_generator.pth",
-		dir: "app/pretrained_weights/liveportrait/base_models"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/base_models/warping_module.pth",
-		dir: "app/pretrained_weights/liveportrait/base_models"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/retargeting_models/stitching_retargeting_module.pth",
-		dir: "app/pretrained_weights/liveportrait/retargeting_models"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/base_models/appearance_feature_extractor.pth",
-		dir: "app/pretrained_weights/liveportrait/animals/base_models"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/base_models/motion_extractor.pth",
-		dir: "app/pretrained_weights/liveportrait/animals/base_models"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/base_models/spade_generator.pth",
-		dir: "app/pretrained_weights/liveportrait/animals/base_models"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/base_models/warping_module.pth",
-		dir: "app/pretrained_weights/liveportrait/animals/base_models"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/retargeting_models/stitching_retargeting_module.pth",
-		dir: "app/pretrained_weights/liveportrait/animals/retargeting_models"
-		}
-	},{
-		method: "fs.download",
-		params: {
-		uri: "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/xpose.pth",
-		dir: "app/pretrained_weights/liveportrait/animals"
-		}
+ run: [
+	{
+	  method: "shell.run",
+	  params: {
+		message: [
+		  "git clone --single-branch --branch animals https://github.com/Lytanshade/LivePortrait.git app ",
+		]
+	  }
 	},
-  
-  
+
+	{
+	  method: "script.start",
+	  params: {
+		uri: "torch.js",
+		params: {
+		  venv: "env",                
+		  path: "app",               
+		  // xformers: true   
+		}
+	  }
+	},
+
+	{
+	  method: "shell.run",
+	  params: {
+		venv: "env",                
+		path: "app",                
+		message: [
+		  "pip install gradio devicetorch",
+		  "pip install -r {{platform==='darwin' ? 'requirements_macOS.txt' : 'requirements.txt'}}"
+		]
+	  }
+	},
+	
+	{
+	  method: "fs.download",
+	  params: {
+	    uri: [
+		  "https://huggingface.co/lytanshade/insightface/resolve/main/det_10g.onnx",
+		  "https://huggingface.co/lytanshade/insightface/resolve/main/2d106det.onnx"
+	    ],
+	    dir: "app/pretrained_weights/insightface/models/buffalo_l"
+	  }
+    },
+    {
+      method: "fs.download",
+	  params: {
+	    uri: "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/landmark.onnx",
+	    dir: "app/pretrained_weights/liveportrait"
+	  }
+    },
+    {
+	  method: "fs.download",
+	  params: {
+	    uri: [
+		  "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/base_models/appearance_feature_extractor.pth",
+		  "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/base_models/motion_extractor.pth",
+		  "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/base_models/spade_generator.pth",
+		  "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/base_models/warping_module.pth"
+	    ],
+	    dir: "app/pretrained_weights/liveportrait/base_models"
+	  }
+    },
+    {
+	  method: "fs.download",
+	  params: {
+	    uri: "https://huggingface.co/lytanshade/liveportrait/resolve/main/liveportrait/retargeting_models/stitching_retargeting_module.pth",
+	    dir: "app/pretrained_weights/liveportrait/retargeting_models"
+	  }
+    },
+    {
+	  method: "fs.download",
+	  params: {
+	    uri: [
+		  "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/base_models/appearance_feature_extractor.pth",
+		  "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/base_models/motion_extractor.pth",
+		  "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/base_models/spade_generator.pth",
+		  "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/base_models/warping_module.pth"
+	    ],
+	    dir: "app/pretrained_weights/liveportrait/animals/base_models"
+	  }
+    },
+    {
+	  method: "fs.download",
+	  params: {
+	    uri: "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/retargeting_models/stitching_retargeting_module.pth",
+	    dir: "app/pretrained_weights/liveportrait/animals/retargeting_models"
+	  }
+    },
+     {
+	  method: "fs.download",
+	  params: {
+	    uri: "https://huggingface.co/lytanshade/liveportrait_animals/resolve/main/xpose.pth",
+	    dir: "app/pretrained_weights/liveportrait/animals"
+	  }
+    },  
+
     {
       method: "notify",
       params: {
