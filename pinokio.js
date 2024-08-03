82ee0f177f33dff84636490c02ff002e7e26c251
Lytan <116470525+Lytanshade@users.noreply.github.com>
2024-08-03 14:18:02 +1200

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports = {
   version: "1.5",
   title: "LivePortrait",
-  description: "LivePortrait: [Nvidia and MacOS silicon only atm] 6GB VRAM & ~8GB download",
+  description: "LivePortrait - animals update: 6GB VRAM & ~8GB download",
   icon: "icon.png",
   menu: async (kernel) => {
     let installing = await kernel.running(__dirname, "install.js")
